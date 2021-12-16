947b16e589f837a7e1e16b7ba423af74051390e1
kurniacf <kcf170202@gmail.com>
2021-12-17 03:11:27 +0700

    fix register

--- auth/users/registerGetData.php
+++ auth/users/registerGetData.php
@@ -12,25 +12,17 @@ if (!empty($_POST['id_session_account']) && !empty($_POST['id_country'])) {
     $id_session_account = $_POST['id_session_account'];
     $id_country = $_POST['id_country'];
 
-    $query = "SELECT * FROM Account WHERE id_session_account = '$id_session_account'";
-    $get = pg_query($connect, $query);
+    $query1 = "SELECT email_account FROM Account WHERE id_session_account = '$id_session_account'";
+    $get1 = pg_query($connect, $query1);
+    $data1 = pg_fetch_row($get1);
+    $email_account = array_pop($data1);
 
-    if (pg_num_rows($get)) {
-        $query1 = "SELECT email_account FROM Account WHERE id_session_account = '$id_session_account'";
-        $get1 = pg_query($connect, $query1);
-        $data1 = pg_fetch_row($get1);
-        $email_account = array_pop($data1);
-
-        $query = "SELECT Account.id_account, Account.id_session_account, Account.name_account, Account.username_account, Account.email_account,
+    $query = "SELECT Account.id_account, Account.id_session_account, Account.name_account, Account.username_account, Account.email_account,
                     Contact.id_contact, Contact.telp_contact,
                     Country.id_country, Country.name_country, Country.iso3_country, Country.phonecode_country
                     FROM Account, Contact
                     JOIN Country ON Country.id_country = '$id_country'
                     WHERE Contact.email_contact = '$email_account' AND  Contact.id_country = '$id_country' AND Account.id_session_account = '$id_session_account'";
-    } else {
-        http_response_code(401);
-        set_response(false, "Data is Not Found", $data);
-    }
 }
 
 $get = pg_query($connect, $query);
