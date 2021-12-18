198ac308f6875923a64c0029582626292eb45bd5
kurniacf <kcf170202@gmail.com>
2021-12-18 21:18:37 +0700

    customer input

--- customer/customerInput.php
+++ customer/customerInput.php
@@ -34,9 +34,8 @@ if (!empty($_POST['id_account']) && !empty($_POST['firstname_customer']) && !emp
             $data = pg_fetch_row($get);
             $id_contact = intval(array_pop($data));
 
-            $query = "INSERT INTO Customer(id_account, id_contact, firstname_customer, lastname_customer, address_customer, type_age_customer, gender_customer, nationality_customer) 
-                            VALUES ('$id_account', '$id_contact','$firstname_customer', '$lastname_customer', '$address_customer', '$type_age_customer', '$gender_customer', '$nationality_customer')";
-            $insert = pg_query($connect, $query);
+            $query3 = "UPDATE Customer set id_account = '$id_account', id_contact = '$id_contact', address_customer = '$address_customer' WHERE firstname_customer = '$firstname_customer' AND lastname_customer = '$lastname_customer' AND type_age_customer = '$type_age_customer' AND gender_customer = '$gender_customer' AND nationality_customer = '$nationality_customer'";
+            $update = pg_query($connect, $query3);
 
             $query = "SELECT id_customer FROM Customer WHERE firstname_customer = '$firstname_customer' AND lastname_customer = '$lastname_customer' AND type_age_customer = '$type_age_customer' AND gender_customer = '$gender_customer' AND nationality_customer = '$nationality_customer'";
             $get = pg_query($connect, $query);
@@ -59,10 +58,10 @@ if (!empty($_POST['id_account']) && !empty($_POST['firstname_customer']) && !emp
                 while ($row = pg_fetch_assoc($getFinal)) {
                     $data[] = $row;
                 }
-                set_response(true, "Data is Found", $data);
+                set_response(true, "Update Data Customer Success", $data);
             } else {
                 http_response_code(400);
-                set_response(false, "Data is Not Found", "Input Data is Wrong!");
+                set_response(false, "Update Data Customer Failed", "Input Data is Wrong!");
             }
         } else {
             $query = "SELECT * FROM Contact WHERE email_contact = '$email_contact' AND id_country = '$id_country'";
@@ -77,43 +76,7 @@ if (!empty($_POST['id_account']) && !empty($_POST['firstname_customer']) && !emp
                 $query3 = "UPDATE Contact set email_contact = '$email_contact', telp_contact = '$telp_contact' WHERE id_contact = '$id_contact'";
                 $update = pg_query($connect, $query3);
 
-                $query = "INSERT INTO Customer(id_account, id_contact, firstname_customer, lastname_customer, address_customer, type_age_customer, gender_customer, nationality_customer) 
-                            VALUES ('$id_account', '$id_contact','$firstname_customer', '$lastname_customer', '$address_customer', '$type_age_customer', '$gender_customer', '$nationality_customer')";
-                $insert = pg_query($connect, $query);
-
-                $query = "SELECT id_customer FROM Customer WHERE firstname_customer = '$firstname_customer' AND lastname_customer = '$lastname_customer' AND type_age_customer = '$type_age_customer' AND gender_customer = '$gender_customer' AND nationality_customer = '$nationality_customer'";
-                $get = pg_query($connect, $query);
-                $data = pg_fetch_row($get);
-                $id_customer = intval(array_pop($data));
-
-                $queryFinal = "SELECT Customer.id_customer, Customer.firstname_customer, Customer.lastname_customer, Customer.address_customer, Customer.type_age_customer, Customer.gender_customer, Customer.nationality_customer,
-                    Account.id_account, Account.id_session_account, Account.name_account, Account.username_account, Account.email_account,
-                    Contact.id_contact, Contact.email_contact, Contact.telp_contact,
-                    Country.id_country, Country.name_country, Country.iso3_country, Country.phonecode_country
-                    FROM Account, Contact
-                    JOIN Customer ON Customer.id_customer = '$id_customer'
-                    JOIN Country ON Country.id_country = '$id_country'
-                    WHERE Contact.email_contact = '$email_contact' AND  Contact.id_country = '$id_country' AND Account.id_account = '$id_account'";
-
-                $getFinal = pg_query($connect, $queryFinal);
-                $data = array();
-
-                if (pg_num_rows($getFinal)) {
-                    while ($row = pg_fetch_assoc($getFinal)) {
-                        $data[] = $row;
-                    }
-                    set_response(true, "Data is Found", $data);
-                } else {
-                    http_response_code(400);
-                    set_response(false, "Data is Not Found", "Input Data is Wrong!");
-                }
-            } else {
-                $query = "SELECT id_contact FROM Customer WHERE id_account = '$id_account' AND firstname_customer = '$firstname_customer' AND lastname_customer = '$lastname_customer' AND type_age_customer = '$type_age_customer' AND gender_customer = '$gender_customer' AND nationality_customer = '$nationality_customer'";
-                $get = pg_query($connect, $query);
-                $data = pg_fetch_row($get);
-                $id_contact = intval(array_pop($data));
-
-                $query3 = "UPDATE Contact set email_contact = '$email_contact', telp_contact = '$telp_contact' WHERE id_contact = '$id_contact'";
+                $query3 = "UPDATE Customer set id_account = '$id_account', id_contact = '$id_contact', address_customer = '$address_customer' WHERE firstname_customer = '$firstname_customer' AND lastname_customer = '$lastname_customer' AND type_age_customer = '$type_age_customer' AND gender_customer = '$gender_customer' AND nationality_customer = '$nationality_customer'";
                 $update = pg_query($connect, $query3);
 
                 $query = "SELECT id_customer FROM Customer WHERE firstname_customer = '$firstname_customer' AND lastname_customer = '$lastname_customer' AND type_age_customer = '$type_age_customer' AND gender_customer = '$gender_customer' AND nationality_customer = '$nationality_customer'";
@@ -137,10 +100,48 @@ if (!empty($_POST['id_account']) && !empty($_POST['firstname_customer']) && !emp
                     while ($row = pg_fetch_assoc($getFinal)) {
                         $data[] = $row;
                     }
-                    set_response(true, "Data is Found", $data);
+                    set_response(true, "Update Data Customer Success", $data);
                 } else {
                     http_response_code(400);
-                    set_response(false, "Data is Not Found", "Input Data is Wrong!");
+                    set_response(false, "Update Data Customer Failed", "Input Data is Wrong!");
+                }
+            } else {
+                $query = "SELECT id_contact FROM Customer WHERE id_account = '$id_account' AND firstname_customer = '$firstname_customer' AND lastname_customer = '$lastname_customer' AND type_age_customer = '$type_age_customer' AND gender_customer = '$gender_customer' AND nationality_customer = '$nationality_customer'";
+                $get = pg_query($connect, $query);
+                $data = pg_fetch_row($get);
+                $id_contact = intval(array_pop($data));
+
+                $query3 = "UPDATE Contact set email_contact = '$email_contact', telp_contact = '$telp_contact' WHERE id_contact = '$id_contact'";
+                $update = pg_query($connect, $query3);
+
+                $query3 = "UPDATE Customer set id_account = '$id_account', id_contact = '$id_contact', address_customer = '$address_customer' WHERE firstname_customer = '$firstname_customer' AND lastname_customer = '$lastname_customer' AND type_age_customer = '$type_age_customer' AND gender_customer = '$gender_customer' AND nationality_customer = '$nationality_customer'";
+                $update = pg_query($connect, $query3);
+
+                $query = "SELECT id_customer FROM Customer WHERE firstname_customer = '$firstname_customer' AND lastname_customer = '$lastname_customer' AND type_age_customer = '$type_age_customer' AND gender_customer = '$gender_customer' AND nationality_customer = '$nationality_customer'";
+                $get = pg_query($connect, $query);
+                $data = pg_fetch_row($get);
+                $id_customer = intval(array_pop($data));
+
+                $queryFinal = "SELECT Customer.id_customer, Customer.firstname_customer, Customer.lastname_customer, Customer.address_customer, Customer.type_age_customer, Customer.gender_customer, Customer.nationality_customer,
+                    Account.id_account, Account.id_session_account, Account.name_account, Account.username_account, Account.email_account,
+                    Contact.id_contact, Contact.email_contact, Contact.telp_contact,
+                    Country.id_country, Country.name_country, Country.iso3_country, Country.phonecode_country
+                    FROM Account, Contact
+                    JOIN Customer ON Customer.id_customer = '$id_customer'
+                    JOIN Country ON Country.id_country = '$id_country'
+                    WHERE Contact.email_contact = '$email_contact' AND  Contact.id_country = '$id_country' AND Account.id_account = '$id_account'";
+
+                $getFinal = pg_query($connect, $queryFinal);
+                $data = array();
+
+                if (pg_num_rows($getFinal)) {
+                    while ($row = pg_fetch_assoc($getFinal)) {
+                        $data[] = $row;
+                    }
+                    set_response(true, "Update Data Customer Success", $data);
+                } else {
+                    http_response_code(400);
+                    set_response(false, "Update Data Customer Failed", "Input Data is Wrong!");
                 }
             }
         }
@@ -179,10 +180,10 @@ if (!empty($_POST['id_account']) && !empty($_POST['firstname_customer']) && !emp
                 while ($row = pg_fetch_assoc($getFinal)) {
                     $data[] = $row;
                 }
-                set_response(true, "Data is Found", $data);
+                set_response(true, "Create Customer Success", $data);
             } else {
                 http_response_code(400);
-                set_response(false, "Data is Not Found", "Input Data is Wrong!");
+                set_response(false, "Create Customer Failed", "Input Data is Wrong!");
             }
         } else {
             $query = "SELECT * FROM Contact WHERE email_contact = '$email_contact' AND telp_contact = '$telp_contact' AND id_country = '$id_country'";
@@ -219,10 +220,10 @@ if (!empty($_POST['id_account']) && !empty($_POST['firstname_customer']) && !emp
                     while ($row = pg_fetch_assoc($getFinal)) {
                         $data[] = $row;
                     }
-                    set_response(true, "Data is Found", $data);
+                    set_response(true, "Create Customer Success", $data);
                 } else {
                     http_response_code(400);
-                    set_response(false, "Data is Not Found", "Input Data is Wrong!");
+                    set_response(false, "Create Customer Failed", "Input Data is Wrong!");
                 }
             } else {
                 $query = "SELECT * FROM Contact WHERE email_contact = '$email_contact' AND id_country = '$id_country'";
@@ -262,10 +263,10 @@ if (!empty($_POST['id_account']) && !empty($_POST['firstname_customer']) && !emp
                         while ($row = pg_fetch_assoc($getFinal)) {
                             $data[] = $row;
                         }
-                        set_response(true, "Data is Found", $data);
+                        set_response(true, "Create Customer Success", $data);
                     } else {
                         http_response_code(400);
-                        set_response(false, "Data is Not Found", "Input Data is Wrong!");
+                        set_response(false, "Create Customer Failed", "Input Data is Wrong!");
                     }
                 } else {
                     $query = "INSERT INTO Contact(email_contact, telp_contact, id_country) 
@@ -302,10 +303,10 @@ if (!empty($_POST['id_account']) && !empty($_POST['firstname_customer']) && !emp
                         while ($row = pg_fetch_assoc($getFinal)) {
                             $data[] = $row;
                         }
-                        set_response(true, "Data is Found", $data);
+                        set_response(true, "Create Customer Success", $data);
                     } else {
                         http_response_code(400);
-                        set_response(false, "Data is Not Found", "Input Data is Wrong!");
+                        set_response(false, "Create Customer Failed", "Input Data is Wrong!");
                     }
                 }
             }
